d8934f74b0a86b241e65f1695f42a7295b91d6f5
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-11-22 20:25:36 +0530

    Add variables for VPC and subnet IDs

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+#variables.tf
+variable "vpc_id" {
+  type    = string
+  default = "vpc-01a8c005fb28bb904"
+}
+
+variable "subnet_id" {
+  type    = string
+  default = "subnet-0374ae17747e0d9cd"
+}
